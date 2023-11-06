00429ca555d5178d5f33cb17789d84e2e151a8d2
tiny_bear <rebeccawilliams9941@gmail.com>
2023-11-07 10:16:37 +1100

    :sparkles: Create join statement for creating prompt data.

--- main.py
+++ main.py
@@ -3,7 +3,22 @@ def get_columns(headers):
     # Creates dict where each header is associated with alphabet key like in G-Sheets
     return {chr(65 + i): header for i, header in enumerate(header_list)}
 
+def join(columns, excluding):
+    exclude = excluding.split()
+    join_statements = []
+    for key, value in columns.items():
+        if key not in exclude:
+            join_statements.append(f'IF({key}2<>"", "{value}: " & {key}2, "")')
+    join_statements = ', '.join(join_statements)
+    return f"""
+    =TEXTJOIN(CHAR(10),
+      TRUE,
+      {join_statements}
+    )
+    """
+
 headers = "slug name payment-methods facilities accreditations open_saturday open_sunday after_hours wheelchair_access accepts_dva_card accepts_healthcare_card billing telehealth languages_spoken main_category suburb state postcode city"
 columns = get_columns(headers)
 print(columns)
-print(columns['A'])
+
+print(join(columns, "A R"))
